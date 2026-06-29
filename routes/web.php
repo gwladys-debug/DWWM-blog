84b96f5c1af0d8a2d2285285c94b990236634a6c
Gwladys Gely <gwladysgely@gmail.com>
2026-06-29 14:08:59 -0400

    Add home view template with basic HTML structure and assets

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/articles', [ArticleController::class,'show']);
+
 Route::get('/categories', [CategoryController::class, 'index']);
