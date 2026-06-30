6744be4646fc1405ddd2e53d3dfc04cf05cb1515
Gwladys Gely <gwladysgely@gmail.com>
2026-06-30 15:37:47 -0400

    Nettoyage du code : suppression des lignes vides inutiles dans la migration de la table des catégories

--- database/migrations/2026_06_30_182528_create_categories_table.php
+++ database/migrations/2026_06_30_182528_create_categories_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    
+
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
@@ -24,6 +24,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
+    
     public function down(): void
     {
         Schema::dropIfExists('categories');
