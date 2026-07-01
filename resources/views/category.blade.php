7e7d80b7ef1159ab6df0563c3ac1a2665c245361
Gwladys Gely <gwladysgely@gmail.com>
2026-07-01 08:37:57 -0400

    maj category.blade

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,10 +1,18 @@
-<ul>
-    @foreach ($categories as $category)
-        <li>
-            <a href="/categories/{{ $category->slug }}">
-                {{ $category->name }}
-            </a>
-        </li>
-    @endforeach
-</ul>
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        // 1. On récupère toutes les catégories de la table SQL
+        $categories = Category::all();
+
+        // 2. On retourne la vue (ex: index.blade.php) en lui passant les catégories
+        return view('blog.index', compact('categories'));
+    }
+}
