2cd4ad6bc3cfa947d532df72136d8f1d94840ac3
Gwladys Gely <gwladysgely@gmail.com>
2026-07-02 14:03:50 -0400

    création du fichier categogy-list.blade.php pour l'affichage des catégories

--- resources/views/categories/categogy-list.blade.php
+++ resources/views/categories/categogy-list.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Catégories</title>
-    <!-- Tu pourras lier ton CSS ou Tailwind ici plus tard -->
+    <!-- Lier le CSS ou Tailwind ici plus tard -->
 </head>
 
 <body>
