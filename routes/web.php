e30a21a780b5499d3440eb144b63ad36fb6cefe8
gwladys-debug <gwladysgely@gmail.com>
2026-07-03 12:21:32 -0400

    Revert "Feature category"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/categories', [CategoryController::class, 'index']);
