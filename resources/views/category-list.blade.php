dbcaadf86cb5d64f9f418c7d88bb5e5c6c52ac25
Gwladys Gely <gwladysgely@gmail.com>
2026-07-08 09:44:45 -0400

    Correction de la langue de la page catégorie : passage de l'anglais au français.

--- resources/views/category-list.blade.php
+++ resources/views/category-list.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -19,19 +19,3 @@
 </body>
 
 </html>
-<?php namespace App\Http\Controllers;
-
-use App\Models\Category;
-use Illuminate\Http\Request;
-
-class BlogController extends Controller
-{
-    public function index()
-    {
-        // 1. On récupère toutes les catégories de la table SQL
-        $categories = Category::all();
-
-        // 2. On retourne la vue (ex: index.blade.php) en lui passant les catégories
-        return view('blog.index', compact('categories'));
-    }
-}
