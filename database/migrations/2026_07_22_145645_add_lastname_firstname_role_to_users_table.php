6dedc65f12694775e5c5e379ec3b6402737763c6
Gwladys Gely <gwladysgely@gmail.com>
2026-07-22 11:40:24 -0400

    Ajout du contrôleur d'inscription et de la vue associée. Mise à jour du modèle User pour inclure les champs lastname, firstname et role. Création de la migration pour modifier la table users. Ajout des routes pour l'inscription des utilisateurs.

---
+++ database/migrations/2026_07_22_145645_add_lastname_firstname_role_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('lastname',50)->after('id');
+            $table->string('firstname',50)->after('lastname');
+            $table->string('role',50)->after('email')->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name',50)->after('id');
+            $table->dropColumn('lastname');
+            $table->dropColumn('firstname');
+            $table->dropColumn('role');
+        });
+    }
+};
